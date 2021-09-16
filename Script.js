7e7c00b5cfb201cc7b13bf5530ee95e17c1ab454
ganesh1172 <ganesh172595@gmail.com>
2021-09-16 18:57:51 +0530

    styled sections

--- Script.js
+++ Script.js
@@ -1,7 +1,24 @@
-const accordion = document.getElementsByClassName("accord-card");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+const accordion = document.getElementsByClassName("accord-card");  
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 
 for(let i=0;i<accordion.length;i++){
     accordion[i].addEventListener('click', function(){
         this.classList.toggle('active');
     })
 }
+
